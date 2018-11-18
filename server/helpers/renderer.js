d642c080794e31c3c2d343de593d62c4b4a390fe
Theodoros Itzaris <theodoreme@gmail.com>
2018-11-18 18:03:08 +0200

    Add server code - redux run on server side - init store is created - page rendered on server & sent to the browser - client bundle sent to the browser - bundle creates its client side store - page rendered with store from client side redux

---
+++ server/helpers/renderer.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import { StaticRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { renderRoutes } from 'react-router-config'
+import serialize from 'serialize-javascript'
+
+import Routes from '../../client/components/Router/routes'
+
+export default (req, store, context = {}) => {
+  const content = renderToString(
+    <Provider store={store}>
+      <StaticRouter context={context} location={req.path}>
+        <div>{renderRoutes(Routes)}</div>
+      </StaticRouter>
+    </Provider>,
+  )
+
+  return (
+    `<!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="UTF-8">
+        <title>Server Side render application</title>
+      </head>
+     <body>
+       <div id="root">${content}</div>
+       <script>
+         window.INITIAL_STATE = ${serialize(store.getState())}
+       </script>
+       <script src="bundle.js"></script>
+     </body>
+    </html>`
+  )
+}
---
+++ server/index.js
@@ -0,0 +1,28 @@
+import '@babel/polyfill'
+import express from 'express'
+import { matchRoutes } from 'react-router-config'
+import createStore from './helpers/createStore'
+import Routes from '../client/components/Router/routes'
+import renderer from './helpers/renderer'
+
+const app = express()
+const PORT = 5000
+
+app.use(express.static('public'))
+
+app.get('*', (req, res) => {
+  const store = createStore()
+  // Initialize and load data into the store
+  const promises = matchRoutes(Routes, req.path).map(({ route }) => {
+    return route.loadData ? route.loadData(store) : null
+  })
+
+  Promise.all(promises).then(() => {
+    const context = {}
+    const content = renderer(req, store, context)
+
+    return res.send(content)
+  })
+})
+
+app.listen(PORT, () => console.log('Server is listening on PORT ', PORT))
