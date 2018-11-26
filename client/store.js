727ebe88a99059238d359aa70e16eb8382c15e34
Theodoros Itzaris <theodoreme@gmail.com>
2018-11-26 20:52:35 +0200

    Dispatch action 'CLEAR_RESULTS' when searchString.length is 1

--- client/store.js
+++ client/store.js
@@ -8,12 +8,13 @@ import thunk from 'redux-thunk'
 
 import rootReducers from './combinedReducers'
 import logger from './middlewares/loger'
+import clearList from './middlewares/clearList'
 
 // initial state coming from the server
 const initialState = window.INITIAL_STATE
 
 const enhancers = []
-const middlewares = [thunk, logger]
+const middlewares = [thunk, logger, clearList]
 
 
 if (process.env.NODE_ENV === 'development') {
